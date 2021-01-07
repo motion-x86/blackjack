e5228bb1b7a36c1bb060a60766bbd1f00a3711aa
marc-villanueva <mvillanueva@fieldlevel.com>
2021-01-07 14:28:36 -0800

    move Alert component into separate folder

--- src/components/shared/Alert/Alert.jsx
+++ src/components/shared/Alert/Alert.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 import styles from './Alert.module.scss';
 
@@ -7,10 +8,15 @@ const Alert = ({ type, children }) => {
     const className = cx(
         styles.Container,
         type === 'success' && styles.success,
-        type === 'critical' && styles.critical
+        type === 'critical' && styles.critical,
+        type === 'default' && styles.default
     );
 
     return <div className={className}>{children}</div>;
+};
+Alert.propTypes = {
+    type: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
 };
 
 export { Alert };
