52410f519ace2bbfcfc631945cf3686c97d3726d
Jesse Myers <myers.dev.acc@gmail.com>
2021-01-14 07:28:17 -0800

    added a flipped card
    -

--- src/components/PlayingCard/PlayingCard.jsx
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -4,15 +4,13 @@ import PropTypes from 'prop-types';
 import styles from './PlayingCard.module.scss';
 
 const PlayingCard = ({ card, faceUp }) => {
-    const code = faceUp ? card.code : 'XX';
+    const imgSrc = faceUp
+        ? `https://deckofcardsapi.com/static/img/${card.code}.png`
+        : 'https://i.pinimg.com/originals/09/a5/8d/09a58d561b2a7b92bd506c83414ef1ab.png';
 
     return (
         <div className={styles.Card}>
-            <img
-                className={styles.CardImg}
-                alt={card.toString()}
-                src={`https://deckofcardsapi.com/static/img/${code}.png`}
-            />
+            <img className={styles.CardImg} alt={card.toString()} src={imgSrc} />
         </div>
     );
 };
