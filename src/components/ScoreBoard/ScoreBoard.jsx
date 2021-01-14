3b0594b1e949f9e54b39395fff619105c6a61b73
Jesse Myers <myers.dev.acc@gmail.com>
2021-01-13 20:23:31 -0800

    added sore board component

---
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './ScoreBoard.module.scss';
+
+const ScoreItem = ({ label, value }) => (
+    <div className={styles.ScoreItem}>
+        <span className={styles.ScoreItemLabel}>{label}:</span>
+        <span className={styles.ScoreItemValue}>{value}</span>
+    </div>
+);
+
+const ScoreBoard = ({ win, loose, push }) => {
+    return (
+        <div className={styles.Container}>
+            <ScoreItem label="Win" value={win} />
+            <ScoreItem label="Loose" value={loose} />
+            <ScoreItem label="Push" value={push} />
+        </div>
+    );
+};
+
+ScoreBoard.propTypes = {
+    win: PropTypes.number.isRequired,
+    loose: PropTypes.number.isRequired,
+    push: PropTypes.number.isRequired
+};
+
+export { ScoreBoard };
